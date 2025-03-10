c72e586bf1573eb27f76ec4a6ccc88b95a4cc00a
TobiXT <126273238+TobiXT@users.noreply.github.com>
2025-03-10 14:12:10 +0100

    Rechtsklick provider

---
+++ entry_menue.py
@@ -0,0 +1,74 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class EntryEx(ttk.Entry):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.menu = tk.Menu(self, tearoff=False)
+        self.menu.add_command(label="Kopieren", command=self.popup_copy)
+        self.menu.add_command(label="Ausschneiden", command=self.popup_cut)
+        self.menu.add_separator()
+        self.menu.add_command(label="Einfügen", command=self.popup_paste)
+        self.bind("<Button-3>", self.display_popup)
+
+    def display_popup(self, event):
+        self.menu.post(event.x_root, event.y_root)
+
+    def popup_copy(self):
+        self.event_generate("<<Copy>>")
+
+    def popup_cut(self):
+        self.event_generate("<<Cut>>")
+
+    def popup_paste(self):
+        self.event_generate("<<Paste>>")
+
+class ToolTipp(object):
+
+    def __init__(self, widget, text='widget info'):
+        self.waittime = 500
+        self.wraplength = 180
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.id = None
+        self.tw = None
+
+    def enter(self, event=None):
+        self.schedule()
+
+    def leave(self, event=None):
+        self.unschedule()
+        self.hidetip()
+        
+    def schedule(self):
+        self.unschedule()
+        self.id = self.widget.after(self.waittime, self.showtip)
+
+    def unschedule(self):
+        id = self.id
+        self.id = None
+        if id:
+            self.widget.after_cancel(id)
+
+    def showtip(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert") 
+        x += self.widget.winfo_rootx()
+        y += self.widget.winfo_rootx() 
+        self.tw = tk.Toplevel(self.widget)
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x,y))
+        label = tk.Label(self.tw, text=self.text, justify='left',
+                         background="#ffffff", relief='solid', borderwidth=1,
+                         wraplength=self.wraplength)
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tw
+        self.tw = None
+        if tw:
+            tw.destroy()
